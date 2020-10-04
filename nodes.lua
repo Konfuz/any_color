82c539181d0b076b58a4e3dbc48b831545ba7717
Timon Giese <timon-g@web.de>
2020-10-04 19:48:01 +0200

    support for colorwallmounted nodes

--- nodes.lua
+++ nodes.lua
@@ -7,9 +7,9 @@ function any_color.add_default_palette(nodename)
   if meta.paramtype2 == "none" then
     override = {paramtype2="color", palette='any_color_mode13.png'}
   elseif meta.paramtype2 == "wallmounted" then
-    override = {paramtype2="colorwallmounted"}
+    override = {paramtype2="colorwallmounted", palette='any_color_mode13_16x2.png'}
   elseif meta.paramtype2 == "facedir" then
-    override = {paramtype2 = "colorfacedir", palette='any_color_mode13_8bit.png'}
+    override = {paramtype2 = "colorfacedir", palette='any_color_mode13_8x1.png'}
     minetest.log(nodename.." set to ".. override.paramtype2)
   else
     minetest.log('warning', "[any_color] Invalid param2type could not set pallete for "..nodename)
@@ -22,8 +22,8 @@ end
 
 minetest.register_node('any_color:colorwood',
   { description = "test",
-    palette = "any_color_mode13.png",
-    paramtype2 = "color",
+    palette = "any_color_mode13_16x2.png",
+    paramtype2 = "colorwallmounted",
     place_param2 = 2,
     tiles = {"any_color_base.png"}
   }
