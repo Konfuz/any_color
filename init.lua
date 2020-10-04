f3af4147842244c221ee40bcc97fdfcf03421c59
Timon Giese <timon-g@web.de>
2020-10-05 01:48:32 +0200

    rudimentary config file for custom settings

--- init.lua
+++ init.lua
@@ -7,18 +7,13 @@ local modpath = minetest.get_modpath("any_color")
 dofile(modpath..'/util.lua')
 dofile(modpath..'/nodes.lua')
 dofile(modpath..'/paintcan.lua')
+f, err = loadfile(modpath..'/config.lua')
+if f == nil then
+  f = loadfile(modpath..'/config.lua.default')
+end
+assert(f ~= nil)
+f() -- load config by executing it
 
--- nodes in this table will be overridden with a default_palette unless
--- they already have one
-any_color.palette_overrides ={'wool:white','stairs:stair_snowblock',
-'default:aspen_tree', 'default:coral_skeleton', 'default:ice', 'default:ladder_steel',
-'default:furnace',
-'default:silver_sandstone', 'stairs:slab_silver_sandstone',
-'default:silver_sandstone_block', 'stairs:slab_silver_sandstone_block',
-'default:silver_sandstone_brick', 'stairs:slab_silver_sandstone_brick',
-'default:snowblock', 'default:silver_sand',
-'default:glass','default:stone', 'default:sand', 'default:desert_sand'}
-
-for v, k in ipairs(any_color.palette_overrides) do
+for v, k in ipairs(any_color.config.simple_overrides) do
   any_color.add_default_palette(k)
 end
