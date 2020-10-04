60950baa6ae0fd978bf02318f4719387d2c53d9f
Timon Giese <timon-g@web.de>
2020-10-04 21:34:17 +0200

    Testnodes and default overrides for testing

--- init.lua
+++ init.lua
@@ -10,7 +10,14 @@ dofile(modpath..'/paintcan.lua')
 
 -- nodes in this table will be overridden with a default_palette unless
 -- they already have one
-any_color.palette_overrides ={'wool:white','stairs:stair_snowblock'}
+any_color.palette_overrides ={'wool:white','stairs:stair_snowblock',
+'default:aspen_tree', 'default:coral_skeleton', 'default:ice', 'default:ladder_steel',
+'default:furnace',
+'default:silver_sandstone', 'stairs:slab_silver_sandstone',
+'default:silver_sandstone_block', 'stairs:slab_silver_sandstone_block',
+'default:silver_sandstone_brick', 'stairs:slab_silver_sandstone_brick',
+'default:snowblock', 'default:silver_sand',
+'default:glass'}
 
 for v, k in ipairs(any_color.palette_overrides) do
   any_color.add_default_palette(k)
--- nodes.lua
+++ nodes.lua
@@ -22,10 +22,25 @@ function any_color.add_default_palette(nodename)
 end
 
 minetest.register_node('any_color:colorwood',
-  { description = "test",
+  { description = "any_color Testblock color",
+    palette = "any_color_mode13.png",
+    paramtype2 = "color",
+    tiles = {"any_color_base.png"}
+  }
+)
+
+minetest.register_node('any_color:colorwood_wallmounted',
+  { description = "any_color Testblock wallmounted",
     palette = "any_color_mode13_16x2.png",
     paramtype2 = "colorwallmounted",
-    place_param2 = 2,
+    tiles = {"any_color_base.png"}
+  }
+)
+
+minetest.register_node('any_color:colorwood_facedir',
+  { description = "any_color Testblock colorfacedir",
+    palette = "any_color_mode13_8x1.png",
+    paramtype2 = "colorfacedir",
     tiles = {"any_color_base.png"}
   }
 )
