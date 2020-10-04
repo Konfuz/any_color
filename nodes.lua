78082bc32416b2203daea94c233c1216c288e8ba
Timon Giese <timon-g@web.de>
2020-10-04 23:11:17 +0200

    fixed testnodes being undiggable

--- nodes.lua
+++ nodes.lua
@@ -23,7 +23,9 @@ minetest.register_node('any_color:colorwood',
   { description = "any_color Testblock color",
     palette = "any_color_mode13.png",
     paramtype2 = "color",
-    tiles = {"any_color_base.png"}
+    tiles = {"any_color_base.png"},
+    groups= {oddly_breakable_by_hand=1}
+
   }
 )
 
@@ -31,7 +33,8 @@ minetest.register_node('any_color:colorwood_wallmounted',
   { description = "any_color Testblock wallmounted",
     palette = "any_color_mode13_16x2.png",
     paramtype2 = "colorwallmounted",
-    tiles = {"any_color_base.png"}
+    tiles = {"any_color_base.png"},
+    groups= {oddly_breakable_by_hand=1}
   }
 )
 
@@ -39,6 +42,7 @@ minetest.register_node('any_color:colorwood_facedir',
   { description = "any_color Testblock colorfacedir",
     palette = "any_color_mode13_8x1.png",
     paramtype2 = "colorfacedir",
-    tiles = {"any_color_base.png"}
+    tiles = {"any_color_base.png"},
+    groups= {oddly_breakable_by_hand=1}
   }
 )
