fc6f57df5383639e1916bafbde9ea8cdb4175135
Timon Giese <timon-g@web.de>
2020-10-05 02:18:57 +0200

    Added "the nuclear option"

--- init.lua
+++ init.lua
@@ -9,11 +9,17 @@ dofile(modpath..'/nodes.lua')
 dofile(modpath..'/paintcan.lua')
 f, err = loadfile(modpath..'/config.lua')
 if f == nil then
-  f = loadfile(modpath..'/config.lua.default')
+  f, err = loadfile(modpath..'/config.lua.default')
 end
-assert(f ~= nil)
+assert(f ~= nil, err)
 f() -- load config by executing it
 
-for v, k in ipairs(any_color.config.simple_overrides) do
-  any_color.add_default_palette(k,{})
+if any_color.config.colorize_all then
+  for k in pairs(minetest.registered_nodes) do
+    any_color.add_default_palette(k, {})
+  end
+else
+  for k, v in ipairs(any_color.config.simple_overrides) do
+    any_color.add_default_palette(v,{})
+  end
 end
