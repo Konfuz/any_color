f37c09d1ac932fc61595079d581190f051fdc5ba
Konfuz <timon@bit-bots.de>
2020-10-06 00:38:50 +0200

    Made default-palette a config option moved add_default_palette to util

--- nodes.lua
+++ nodes.lua
@@ -1,24 +1,4 @@
 
-function any_color.add_default_palette(nodename, override)
-  local meta = minetest.registered_nodes[nodename]
-  if (meta == nil) or (meta.palette ~= nil) then return end
-
-  if meta.paramtype2 == "none" then
-    if override.paramtype2 == nil then override.paramtype2 = "color" end
-    if override.palette == nil then override.palette = 'any_color_mode13.png' end
-  elseif meta.paramtype2 == "wallmounted" then
-    if override.paramtype2 == nil then override.paramtype2 = "colorwallmounted" end
-    if override.palette == nil then override.palette = 'any_color_mode13_16x2.png' end
-  elseif meta.paramtype2 == "facedir" then
-    if override.paramtype2 == nil then override.paramtype2 = "colorfacedir" end
-    if override.palette == nil then override.palette = 'any_color_mode13_8x1.png' end
-  else
-    minetest.log('warning', "[any_color] Invalid param2type: "..meta.paramtype.." could not set pallete for "..nodename)
-    return
-  end
-  minetest.log("verbose","[any_color] Setting default_palette for "..nodename)
-  minetest.override_item(nodename, override)
-end
 
 minetest.register_node('any_color:colorwood',
   { description = "any_color Testblock color",
--- util.lua
+++ util.lua
@@ -16,3 +16,38 @@ function any_color.dump(o)
       return tostring(o)
    end
 end
+
+function any_color.add_default_palette(nodename, override)
+  -- sets up nodename with a fitting default palette if possible
+  -- Returns false on error
+  local meta = minetest.registered_nodes[nodename]
+  if (meta == nil) or (meta.palette ~= nil) then return false end
+
+  if meta.paramtype2 == "none" then
+    if override.paramtype2 == nil then
+      override.paramtype2 = "color"
+    end
+    if override.palette == nil then
+      override.palette = any_color.config.default_palette_256
+    end
+  elseif meta.paramtype2 == "wallmounted" then
+    if override.paramtype2 == nil then
+      override.paramtype2 = "colorwallmounted"
+    end
+    if override.palette == nil then
+      override.palette = any_color.config.default_palette_32
+    end
+  elseif meta.paramtype2 == "facedir" then
+    if override.paramtype2 == nil then
+      override.paramtype2 = "colorfacedir"
+    end
+    if override.palette == nil then
+      override.palette = any_color.config.default_palette_8
+    end
+  else
+    minetest.log('warning', "[any_color] Invalid param2type: "..meta.paramtype.." could not set pallete for "..nodename)
+    return false
+  end
+  minetest.log("verbose","[any_color] Setting default_palette for "..nodename)
+  return minetest.override_item(nodename, override)
+end
